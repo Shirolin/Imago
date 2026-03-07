9adbf48a749f802f780e7422958e7a1557f6627f
Shirolin <shirolin.com@gmail.com>
2026-03-07 21:46:30 +0800

    chore: 配置 ESLint, Prettier 并集成至构建流程

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import eslint from '@eslint/js';
+import tsESLint from 'typescript-eslint';
+import vuePlugin from 'eslint-plugin-vue';
+import prettierConfig from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default tsESLint.config(
+  eslint.configs.recommended,
+  ...tsESLint.configs.recommended,
+  ...vuePlugin.configs['flat/essential'],
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        parser: tsESLint.parser,
+      },
+    },
+  },
+  {
+    files: ['*.vue', '**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tsESLint.parser,
+      },
+    },
+  },
+  prettierConfig,
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-undef': 'error'
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**', 'public/**'],
+  }
+);
